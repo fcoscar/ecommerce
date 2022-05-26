5cb08f33fa4695adceb16d21f6a18ad280979308
fcoscar <oscarjr01.13@gmail.com>
2022-05-25 23:36:02 -0400

    Botones Centrados, Funciones de Ordenes

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,3 +1,4 @@
+import { CART_SHIPPING_ADDRESS_RESET } from '../constants/cartConstants'
 import { 
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
@@ -13,7 +14,10 @@ import {
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_RESET,
-    USER_DETAILS_RESET
+    USER_DETAILS_RESET,
+    USER_ORDER_DETAILS_REQUEST,
+    USER_ORDER_DETAILS_SUCCESS,
+    USER_ORDER_DETAILS_FAIL,
 } from '../constants/userConstants'
 
 export const login = (username, password) =>  async (dispatch) => {
@@ -52,26 +56,21 @@ export const login = (username, password) =>  async (dispatch) => {
 
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
+    //localStorage.removeItem('shippingAddress')
     dispatch({type:USER_LOGOUT})
     //Limpiar userDetails cuando hacemos log out y no se quede la info del usuario anterior(userDetailsReducer)
     dispatch({type:USER_DETAILS_RESET})
 
 }
 
-export const userRegister = (first_name, last_name, username, email, password ) => async (dispatch) => {
+export const userRegister = (user) => async (dispatch) => {
     try{
         dispatch({
             type: USER_REGISTER_REQUEST
         })
         const response = await fetch('/users/profile/create/', {
             method: 'POST',
-            body: JSON.stringify({
-                first_name: first_name,
-                last_name: last_name,
-                username: username,
-                email: email,
-                password: password
-            }),
+            body: JSON.stringify(user),
             headers: {
                 'Content-type': 'application/json'
             }
@@ -83,6 +82,7 @@ export const userRegister = (first_name, last_name, username, email, password ) 
             payload: data
         })
         localStorage.setItem('userInfo', JSON.stringify(data))
+        dispatch(login(user.username, user.password))
 
     }catch(error){
         dispatch({
@@ -168,6 +168,39 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     }catch(error){
         dispatch({
             type: USER_UPDATE_PROFILE_FAIL,
+            payload: error.response && error.response.data.message
+            ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const getUserOrders = () => async (dispatch, getState) => {
+    
+    try{
+        dispatch({
+            type: USER_ORDER_DETAILS_REQUEST
+        })
+
+        const { userLogin: {userInfo} } = getState()
+        const token = 'Bearer ' + userInfo.token
+
+        const response = await fetch(`/user/orders/`,{
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': token
+            }
+        })
+        const data = await response.json()
+
+        dispatch({
+            type: USER_ORDER_DETAILS_SUCCESS,
+            payload: data
+        })
+
+    }catch(error){
+        dispatch({
+            type: USER_ORDER_DETAILS_FAIL,
             payload: error.response && error.response.data.message
             ? error.response.data.message : error.message
         })
